b9110da35bc765a5581158940f5e916942d1a532
vpinelcito <286876177+vpinelcito@users.noreply.github.com>
2026-09-13 00:55:23 +0200

    Update test.js

--- script/test.js
+++ script/test.js
@@ -1,4 +1,4 @@
-
+const menu = ''<div id="haut"></div><header><nav><div class="changement_langue"><span id="FR">FR</span>|<span id="EN">EN</span></div><label for="menu" id="deploye"></label><input type="checkbox" id="menu" role="button"><ul class="list_menu"><li><label>'
 
 let langue = "EN";
 const nom = "langue"
