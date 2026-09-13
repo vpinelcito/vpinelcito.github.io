731c1ee2719a138cbb1c4f36304426598d8f8c93
vpinelcito <286876177+vpinelcito@users.noreply.github.com>
2026-09-13 02:02:42 +0200

    Update test.js

--- script/test.js
+++ script/test.js
@@ -1,10 +1,19 @@
 let langue = "fr";
 const nom = "langue"
 
-function code_commun() {
-    const section = document.querySelector("section")
-    section.insertAdjacentHTML("beforebegin", menu[langue]);
-    section.insertAdjacentHTML("afterend",pied_page[langue]);
+async function charger_menus() {
+    try {
+        const reponse = await fetch("../contenu/menus.json");
+        if (!reponse.ok) {
+            throw new Error(`Erreur HTTP ! Statut : ${reponse.status}`);
+        }
+        const donnes = await reponse.json();
+        const section = document.querySelector("section")
+        section.insertAdjacentHTML("beforebegin", donnes[langue]["en_tete"]);
+        section.insertAdjacentHTML("afterend",donnes[langue]["pied_page"]);
+    } catch (erreur) {
+        console.error("Impossible de charger le fichier JSON :", erreur);
+    }
 }
 
-document.addEventListener("DOMContentLoaded",code_commun);
+document.addEventListener("DOMContentLoaded",charger_menus);
