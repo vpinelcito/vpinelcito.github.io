2e5ac07d48f7f647861c0564ed0dc3ba7ecc3aad
vpinelcito <286876177+vpinelcito@users.noreply.github.com>
2026-09-17 01:05:58 +0200

    Add files via upload

--- script/dynamique.js
+++ script/dynamique.js
@@ -1,16 +1,16 @@
 const body = document.querySelector("body");
 body.addEventListener("click", (evenement) => {
     if (evenement.target && evenement.target.id === "FR") {
-        document.cookie ="langue=fr; path=/; domain=vpinelcito.github.io; max-age=604800000; samesite=strict; secure" //domain=vpinelcito.github.io
+        document.cookie ="langue=fr; path=/; max-age=604800000; samesite=strict; secure" //domain=vpinelcito.github.io
         window.location.reload();
     } else if(evenement.target && evenement.target.id === "EN"){
-        document.cookie ="langue=en; path=/; domain=vpinelcito.github.io; max-age=604800000; samesite=strict; secure" //domain=vpinelcito.github.io
+        document.cookie ="langue=en; path=/; max-age=604800000; samesite=strict; secure" //domain=vpinelcito.github.io
         window.location.reload();
     }else if(evenement.target && evenement.target.id === "deploye"){
         document.getElementById(evenement.target.id).classList.toggle("fermeture");
     }else if(evenement.target && evenement.target.id === "cookie"){
         document.getElementById("cookie").style.display = "none";
     }else if(evenement.target && evenement.target.id === "site_iut"){
-        document.location.href = "https://www.iut-cachan.universite-paris-saclay.fr/"
+        document.location.href = "https://www.iut-cachan.universite-paris-saclay.fr/";
     }
 });
